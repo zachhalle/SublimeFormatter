9e66a48c1e0f8e8128788ff607776561bee2e4d2
Zachary Halle <zhalle@cmu.edu>
2015-07-16 16:30:15 -0400

    initial commit

---
+++ formatter.py
@@ -0,0 +1,32 @@
+import sublime, sublime_plugin
+import subprocess
+
+class FormatterCommand(sublime_plugin.TextCommand):
+	
+	def get_extension(self, fname):
+		idx = fname.rfind('.')
+		fname[idx:]
+
+	def run(self, edit):
+		
+		fname = self.view.file_name()
+
+		if not self.view.settings().has('code_indent_paths'):
+			sublime.error_message('Formatter needs the code_indent_paths setting to be configured.')
+			return
+
+		extension = self.get_extension(fname)
+		indent_paths = self.view.settings.get('code_indent_paths')
+
+		if not extension in indent_paths:
+			sublime.error_message('Formatter does not have an indenter configured for the extension ' + extension)
+
+		indent_path = indent_paths[extension]
+		content = sublime.Region(0, self.view.size())
+
+		try:
+			cmd_string = 'path fname'.replace('path', indent_path).replace('fname', fname)
+			indented = subprocess.check_output(cmd_string, shell=True)
+			self.view.replace(edit, content, "".join(map(chr, indented)))
+		except subprocess.CalledProcessError as e:
+			sublime.error_message('Error while running indenter:'.replace(e.output))
