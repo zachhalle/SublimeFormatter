aac2655504152e033c5de9472104750e98206b44
Zachary Halle <zhalle@cmu.edu>
2015-07-16 22:16:52 -0400

    bug fixes

--- formatter.py
+++ formatter.py
@@ -5,21 +5,25 @@ class FormatterCommand(sublime_plugin.TextCommand):
 	
 	def get_extension(self, fname):
 		idx = fname.rfind('.')
-		fname[idx:]
+		return fname[idx:]
+		
 
 	def run(self, edit):
 		
 		fname = self.view.file_name()
 
-		if not self.view.settings().has('code_indent_paths'):
+		if not self.view.settings().has('code_format_paths'):
 			sublime.error_message('Formatter needs the code_indent_paths setting to be configured.')
 			return
 
 		extension = self.get_extension(fname)
-		indent_paths = self.view.settings.get('code_indent_paths')
+		indent_paths = self.view.settings().get('code_format_paths')
 
 		if not extension in indent_paths:
 			sublime.error_message('Formatter does not have an indenter configured for the extension ' + extension)
+			return
+
+
 
 		indent_path = indent_paths[extension]
 		content = sublime.Region(0, self.view.size())
